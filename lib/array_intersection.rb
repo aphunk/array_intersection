1d878301469c552734c1548f9fa10e312d1367e5
Amy Phung <phung.amy85@gmail.com>
2019-04-17 21:07:45 -0700

    intersection method implemented

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,28 @@
-# Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n + m), where n is the length of array1 and m is the length of array2
+# Space complexity: O(n), where n is the smaller of the two arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  common_elements = []
+  if array1 == nil || array2 == nil
+    return common_elements
+  end
+
+  if array1.length < array2.length
+    small = array1
+    big = array2
+  else
+    small = array2
+    big = array1
+  end
+
+  hash = {}
+  small.each do |num|
+    hash[num] = 1
+  end
+
+  big.each do |num1|
+    if hash.include?(num1)
+      common_elements << num1
+    end
+  end
+  return common_elements
 end
